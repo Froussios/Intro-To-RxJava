e29eee65d3a0235947e728277dfd3a1fed4d2098
Chris Froussios <darkmatter.tm@gmail.com>
2015-04-28 17:07:18 +0200

    Added every ReplaySubject example

--- tests/java/itrx/ReplaySubjectTest.java
+++ tests/java/itrx/ReplaySubjectTest.java
@@ -1,25 +1,19 @@
 package itrx;
 
 import java.util.Arrays;
+import java.util.concurrent.TimeUnit;
 
 import org.junit.Test;
 
 import rx.observers.TestSubscriber;
+import rx.schedulers.Schedulers;
+import rx.schedulers.TestScheduler;
 import rx.subjects.ReplaySubject;
 
 public class ReplaySubjectTest {
-
-	public static void main(String[] args) {
-		ReplaySubject<Integer> s = ReplaySubject.create();  
-	    s.subscribe(v -> System.out.println("Early:" + v));
-	    s.onNext(0);
-	    s.onNext(1);
-	    s.subscribe(v -> System.out.println("Late: " + v)); 
-	    s.onNext(2);
-	}
 	
 	@Test
-	public void test() {
+	public void testEarlyLate() {
 		TestSubscriber<Integer> tester = new TestSubscriber<Integer>();
 		
 		ReplaySubject<Integer> s = ReplaySubject.create();  
@@ -30,6 +24,37 @@ public class ReplaySubjectTest {
 	    s.onNext(2);
 	    
 	    tester.assertReceivedOnNext(Arrays.asList(0, 1, 0, 1, 2, 2));
+	}
+	
+	@Test
+	public void testWithSize() {
+		TestSubscriber<Integer> tester = new TestSubscriber<Integer>();
+		
+		ReplaySubject<Integer> s = ReplaySubject.createWithSize(2); 
+		s.onNext(0);
+		s.onNext(1);
+		s.onNext(2);
+		s.subscribe(tester); 
+		s.onNext(3);
+		
+		tester.assertReceivedOnNext(Arrays.asList(1,2,3));
+	}
+	
+	@Test 
+	public void testWithTime() throws InterruptedException {
+		TestSubscriber<Integer> tester = new TestSubscriber<Integer>();
+		TestScheduler scheduler = Schedulers.test();
+		
+		ReplaySubject<Integer> s = ReplaySubject.createWithTime(150, TimeUnit.MILLISECONDS, scheduler);
+		s.onNext(0);
+		scheduler.advanceTimeBy(100, TimeUnit.MILLISECONDS);
+		s.onNext(1);
+		scheduler.advanceTimeBy(100, TimeUnit.MILLISECONDS);
+		s.onNext(2);
+		s.subscribe(tester); 
+		s.onNext(3);
+		
+		tester.assertReceivedOnNext(Arrays.asList(1,2,3));
 	}
 	
 
