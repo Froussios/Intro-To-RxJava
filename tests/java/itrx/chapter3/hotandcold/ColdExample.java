9bd4441e6e90c499e51ea89728b0770093d3eebf
Chris Froussios <darkmatter.tm@gmail.com>
2015-05-07 18:51:42 +0200

    Examples 3.6 Hot and cold observables

---
+++ tests/java/itrx/chapter3/hotandcold/ColdExample.java
@@ -0,0 +1,71 @@
+package itrx.chapter3.hotandcold;
+
+import java.util.Arrays;
+import java.util.concurrent.TimeUnit;
+
+import org.junit.Test;
+
+import rx.Observable;
+import rx.observers.TestSubscriber;
+import rx.schedulers.Schedulers;
+import rx.schedulers.TestScheduler;
+
+public class ColdExample {
+
+	public void example() throws InterruptedException {
+		Observable<Long> cold = 
+				Observable
+					.interval(200, TimeUnit.MILLISECONDS)
+					.take(5);
+
+		cold.subscribe(i -> System.out.println("First: " + i));
+		Thread.sleep(500);
+		cold.subscribe(i -> System.out.println("Second: " + i));
+		
+//		First: 0
+//		First: 1
+//		First: 2
+//		Second: 0
+//		First: 3
+//		Second: 1
+//		First: 4
+//		Second: 2
+//		Second: 3
+//		Second: 4
+	}
+	
+	
+	//
+	// Test
+	//
+	
+	@Test
+	public void test() {
+		TestScheduler scheduler = Schedulers.test();
+		TestSubscriber<Long> tester1 = new TestSubscriber<>();
+		TestSubscriber<Long> tester2 = new TestSubscriber<>();
+		
+		Observable<Long> cold = 
+				Observable
+					.interval(200, TimeUnit.MILLISECONDS, scheduler)
+					.take(5);
+
+		cold.subscribe(tester1);
+		tester1.assertReceivedOnNext(Arrays.asList());
+		tester2.assertReceivedOnNext(Arrays.asList());
+		
+		scheduler.advanceTimeTo(500, TimeUnit.MILLISECONDS);
+		cold.subscribe(tester2);
+		tester1.assertReceivedOnNext(Arrays.asList(0L, 1L));
+		tester2.assertReceivedOnNext(Arrays.asList());
+		
+		scheduler.advanceTimeTo(1000, TimeUnit.MILLISECONDS);
+		tester1.assertReceivedOnNext(Arrays.asList(0L, 1L, 2L, 3L, 4L));
+		tester2.assertReceivedOnNext(Arrays.asList(0L, 1L));
+		
+		scheduler.advanceTimeTo(1500, TimeUnit.MILLISECONDS);
+		tester1.assertReceivedOnNext(Arrays.asList(0L, 1L, 2L, 3L, 4L));
+		tester1.assertReceivedOnNext(Arrays.asList(0L, 1L, 2L, 3L, 4L));
+	}
+
+}
---
+++ tests/java/itrx/chapter3/hotandcold/ReplayTest.java
@@ -0,0 +1,72 @@
+package itrx.chapter3.hotandcold;
+
+import java.util.Arrays;
+import java.util.concurrent.TimeUnit;
+
+import org.junit.Test;
+
+import rx.Observable;
+import rx.Subscription;
+import rx.observables.ConnectableObservable;
+import rx.observers.TestSubscriber;
+import rx.schedulers.Schedulers;
+import rx.schedulers.TestScheduler;
+
+public class ReplayTest {
+
+	public void exampleReplay() throws InterruptedException {
+		ConnectableObservable<Long> cold = Observable.interval(200, TimeUnit.MILLISECONDS).replay();
+		cold.connect();
+
+		System.out.println("Subscribe first");
+		cold.subscribe(i -> System.out.println("First: " + i));
+		Thread.sleep(700);
+		System.out.println("Subscribe second");
+		cold.subscribe(i -> System.out.println("Second: " + i));
+		Thread.sleep(500);
+		
+//		Subscribe first
+//		First: 0
+//		First: 1
+//		First: 2
+//		Subscribe second
+//		Second: 0
+//		Second: 1
+//		Second: 2
+//		First: 3
+//		Second: 3
+	}
+	
+	
+	//
+	// Test
+	//
+	
+	@Test
+	public void testReplay() throws InterruptedException {
+		TestScheduler scheduler = Schedulers.test();
+		TestSubscriber<Long> tester1 = new TestSubscriber<>();
+		TestSubscriber<Long> tester2 = new TestSubscriber<>();
+		
+		ConnectableObservable<Long> cold = 
+				Observable
+					.interval(200, TimeUnit.MILLISECONDS, scheduler)
+					.replay();
+		Subscription connection = cold.connect();
+
+		cold.subscribe(tester1);
+		scheduler.advanceTimeBy(700, TimeUnit.MILLISECONDS);
+		tester1.assertReceivedOnNext(Arrays.asList(0L, 1L, 2L));
+		tester2.assertReceivedOnNext(Arrays.asList());
+		
+		cold.subscribe(tester2);
+		tester1.assertReceivedOnNext(Arrays.asList(0L, 1L, 2L));
+		tester2.assertReceivedOnNext(Arrays.asList(0L, 1L, 2L));
+		scheduler.advanceTimeBy(500, TimeUnit.MILLISECONDS);
+		tester1.assertReceivedOnNext(Arrays.asList(0L, 1L, 2L, 3L, 4L, 5L));
+		tester2.assertReceivedOnNext(Arrays.asList(0L, 1L, 2L, 3L, 4L, 5L));
+		
+		connection.unsubscribe();
+	}
+
+}
