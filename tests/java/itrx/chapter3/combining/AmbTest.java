5c2ed42e0eead327e99177fe1f61d8c33005ae34
Chris Froussios <darkmatter.tm@gmail.com>
2015-05-18 19:10:13 +0200

    3.4 Added `ambWith` example

--- tests/java/itrx/chapter3/combining/AmbTest.java
+++ tests/java/itrx/chapter3/combining/AmbTest.java
@@ -12,12 +12,21 @@ import rx.schedulers.TestScheduler;
 
 public class AmbTest {
 
-	public void example() {
+	public void exampleAmb() {
 		Observable.amb(
 		        Observable.timer(100, TimeUnit.MILLISECONDS).map(i -> "First"),
 		        Observable.timer(50, TimeUnit.MILLISECONDS).map(i -> "Second"))
 		    .subscribe(System.out::println);
 		
+		// Second
+	}
+	
+	public void exampleAmbWith() {
+		Observable.timer(100, TimeUnit.MILLISECONDS).map(i -> "First")
+				.ambWith(Observable.timer(50, TimeUnit.MILLISECONDS).map(i -> "Second"))
+				.ambWith(Observable.timer(70, TimeUnit.MILLISECONDS).map(i -> "Third"))
+			.subscribe(System.out::println);
+        
 		// Second
 	}
 	
@@ -27,7 +36,7 @@ public class AmbTest {
 	//
 	
 	@Test
-	public void test() {
+	public void testAmb() {
 		TestSubscriber<String> tester = new TestSubscriber<>();
 		TestScheduler scheduler = Schedulers.test();
 		
@@ -37,6 +46,22 @@ public class AmbTest {
 		    .subscribe(tester);
 	    
 	    scheduler.advanceTimeBy(100, TimeUnit.MILLISECONDS);
+	    tester.assertReceivedOnNext(Arrays.asList("Second"));
+	    tester.assertTerminalEvent();
+	    tester.assertNoErrors();
+	}
+	
+	@Test
+	public void testAmbWith() {
+		TestSubscriber<String> tester = new TestSubscriber<>();
+		TestScheduler scheduler = Schedulers.test();
+		
+		Observable.timer(100, TimeUnit.MILLISECONDS, scheduler).map(i -> "First")
+				.ambWith(Observable.timer(50, TimeUnit.MILLISECONDS, scheduler).map(i -> "Second"))
+				.ambWith(Observable.timer(70, TimeUnit.MILLISECONDS, scheduler).map(i -> "Third"))
+			.subscribe(tester);
+        
+		scheduler.advanceTimeBy(100, TimeUnit.MILLISECONDS);
 	    tester.assertReceivedOnNext(Arrays.asList("Second"));
 	    tester.assertTerminalEvent();
 	    tester.assertNoErrors();
