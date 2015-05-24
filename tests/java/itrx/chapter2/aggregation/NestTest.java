56351eb612977bae520f1c7a890494c92be50214
Chris Froussios <darkmatter.tm@gmail.com>
2015-05-25 00:27:41 +0200

    2.4 Added `nest` examples

---
+++ tests/java/itrx/chapter2/aggregation/NestTest.java
@@ -0,0 +1,40 @@
+package itrx.chapter2.aggregation;
+
+import java.util.Arrays;
+
+import org.junit.Test;
+
+import rx.Observable;
+import rx.observers.TestSubscriber;
+
+public class NestTest {
+
+	public void example() {
+		Observable.range(0, 3)
+			.nest()
+			.subscribe(ob -> ob.subscribe(System.out::println));
+		
+		// 0
+		// 1
+		// 2
+	}
+	
+	
+	//
+	// Test
+	//
+	
+	@Test
+	public void test() {
+		TestSubscriber<Integer> tester = new TestSubscriber<>();
+		
+		Observable.range(0, 3)
+			.nest()
+			.subscribe(ob -> ob.subscribe(tester));
+		
+		tester.assertReceivedOnNext(Arrays.asList(0,1,2));
+		tester.assertTerminalEvent();
+		tester.assertNoErrors();
+	}
+
+}
